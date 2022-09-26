6b4ac34a805d5bc02bd34f5b1c8c6d8f3c3c6a31
Timibreez <ffftimi@gmail.com>
2022-09-26 10:35:00 +0100

    Program.cs: setup user input for fibonacci sequence

--- Program.cs
+++ Program.cs
@@ -1,14 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Please enter an Integer to find its Fibonacci Series: ");
-int result = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(Fibonacci(result));
+﻿Console.Write("Please Enter a Positive Integer to get its Fibonacci series: ");
+int result = Fibonacci(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine(result);
 
 static int Fibonacci(int n){
     int n1 = 0;
     int n2 = 1;
     int sum;
 
-    for(int i = 2; i < n; i++){
+    for(int i = 2; i <= n; i++){
         sum = n1 + n2;
         n1 = n2;
         n2 = sum;
