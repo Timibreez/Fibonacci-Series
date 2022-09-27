ebdabe03c96d062ce619d0b2b56e2613ee2a62cb
Timibreez <ffftimi@gmail.com>
2022-09-27 11:16:25 +0100

    Modifying Program.cs: Testing out Debug.Assert - note that the fibonacci of the number 5 is investigated, but the assert comparism of n2 is 6 not 5, hence I expected a break with a new Debug message from assert

--- Program.cs
+++ Program.cs
@@ -1,22 +1,25 @@
 ﻿using System.Diagnostics;
 
 Debug.Write("Please Enter a Positive Integer to get its Fibonacci series: ");
-int n = Fibonacci(Convert.ToInt32(Console.ReadLine()));
-Debug.WriteLine($"We are looking for the {n}th number");
-Console.WriteLine(n);
 
-Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
-static int Fibonacci(int num){
+int result = Fibonacci(6);
+
+Console.WriteLine(result);
+
+static int Fibonacci(int n){
+    Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+    Debug.WriteLine($"We are looking for the {n}th number");
     int n1 = 0;
     int n2 = 1;
     int sum;
 
-    for(int i = 2; i <= num; i++){
+    for(int i = 2; i <= n; i++){
         sum = n1 + n2;
         n1 = n2;
         n2 = sum;
         Debug.WriteLineIf(sum == 1, $"sum is 1, n1 is {n1}, n2 is {n2}");
     }
 
-    return num == 0? n1:n2;
+    Debug.Assert(n2 == 5, "The return value should be 5 and it is not.");
+    return n == 0? n1:n2;
 }
